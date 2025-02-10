054f8e17b4b8674924ed4c0322554131032cf3e2
rishad n <rishad.n@codinghands.in>
2025-02-10 20:44:01 +0530

    implement string reversal functionality and add .gitignore

--- 1.reversed_string/src/main.rs
+++ 1.reversed_string/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+/// Reads a line of input from the user and prints it back out in reverse order.
+///
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    println!("Enter a string:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let input = input.trim().chars().rev().collect::<String>();
+    println!("{}", input);
 }
