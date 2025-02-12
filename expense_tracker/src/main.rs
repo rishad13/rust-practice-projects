af06fc4b49dbb68ba2e27a5983e22ec5004fa2ed
rishad.n <rishad.n@codinghands.in>
2025-02-12 17:16:25 +0530

    Implement basic expense tracker menu with options to enter, view, and exit

--- expense_tracker/src/main.rs
+++ expense_tracker/src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("1:Enter expense (amount category description):");
+        println!("2: View all expenses");
+        println!("3: Exit");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        match input.trim() {
+            "1" => println!("Enter expense (amount category description):"),
+            "2" => println!("View all expenses"),
+            "3" => {
+                println!("Bye Bye");
+                break;
+            }
+            _ => println!("Invalid input"),
+        }
+    }
 }
